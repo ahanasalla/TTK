f7996d04469788ad9ecf21b4ddb323b2ddfee4a7
Ahana <ahanareddy@gmail.com>
2017-09-18 10:12:46 -0700

    hot-lunch-american

--- wp-content/themes/accesspress_parallax_pro-child/functions.php
+++ wp-content/themes/accesspress_parallax_pro-child/functions.php
@@ -165,6 +165,10 @@ function wh_checkAndUpdateCart()
         {
             WC()->cart->set_quantity($cart_item_key, 20);
         }
+        else if (has_term('hot-lunch-american', 'product_tag', $product_id) && ($product_qty < 20))
+        {
+            WC()->cart->set_quantity($cart_item_key, 20);
+        }
     }
 }
 
